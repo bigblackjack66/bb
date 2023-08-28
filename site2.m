08b1eb635b64517451638a7e3fb9f29cf8c8ec5a
bigblackjack66 <141538752+bigblackjack66@users.noreply.github.com>
2023-08-28 13:18:54 -0300

    Create site2.m

---
+++ site2.m
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meus Livros</title>
+  <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+  <header>
+    <h1>Minha Coleção de Livros</h1>
+  </header>
+
+  <main>
+    <section class="add-book">
+      <h2>Adicionar Livro</h2>
+      <input type="text" id="book-title" placeholder="Título do livro">
+      <button id="add-button">Adicionar</button>
+    </section>
+
+    <section class="book-list">
+      <h2>Meus Livros</h2>
+      <ul id="book-list"></ul>
+    </section>
+  </main>
+   <script src="roteiro.js"></script>
+</body>
+</html>
+
+  <script src="roteiro.js"></script>
+</body>
+</html>
